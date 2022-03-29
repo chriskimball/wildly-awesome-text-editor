5a1957b38a542c1beb50c77b86b0fd5e3d86eb78
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-03-28 18:27:59 -0700

    Adjusted database and editor to correctly save values to indexedb.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,8 @@ const initdb = async () =>
     },
   });
 
-// Add logic to a method that accepts some content and adds it to the database
-export const postDb = async (content) => {
+// Add logic to a method that accepts some content and UPDATES it to the database
+export const putDb = async (content) => {
   console.log('Post to the database');
 
   // Create a connection to the database database and version we want to use.
@@ -26,7 +26,7 @@ export const postDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ content: content });
+  const request = store.put({  content: content , id: 1});
 
   // Get confirmation of the request.
   const result = await request;
